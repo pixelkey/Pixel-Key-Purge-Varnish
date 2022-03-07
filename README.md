becf2a66d8a4457814af99e805404e15560a7a79
Andrew Greirson <andrew@pixelkey.com.au>
2022-03-07 15:55:19 +1030

    Update purge-varnish-cache
    Create purge-varnish-cache.php
    
    Update purge-varnish-cache.php
    
    Update purge-varnish-cache.php
    
    Update README.md
    
    Update purge-varnish-cache.php
    
    Update purge-varnish-cache.php

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Pixel Key Purge Varnish
  Purge Vanish Cache on post save/update with cURL method.
+
+ You can also add the action for post status transition to purge varnish cache. However, this may be overkill in some most cases. Hence it has been disabled unless you need it.
+
+ Set cURL options and header according to your server requirements.
+ There are two methods supplied: BAN and PURGE
+ By default it is set to BAN.
+
+ It is recommended to place this plugin file in the mu-plugins folder, where it will be automatically included by WordPress as a "Must Use" Plugin. Aternatively, create a folder with the plugin name, place the file in the folder and include it the plugins folder.
+
+ For testing, uncomment debugging sections. A curl_status_log file will be created in current plugins folder - mu-plugins folder.
+
+
+
+
---
+++ purge-varnish-cache.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Plugin Name: Pixel Key Purge Varnish
+ * Description: Purge Vanish Cache on post save/update with curl method.
+ * Version: 1.0.0
+ * Author: Pixel Key
+ * Author URI: https://pixelkey.com
+ * Requires PHP: 7.2.0
+ */
+
+
+namespace pixelkey_purge_varnish;
+
+class PostHooks
+{
+	public static function init()
+	{
+		// Add action to save post to purge varnish cache
+		add_action('save_post', [static::class, 'onPostSaveAndUpdate']);
+
+		// // Add action to post status transition to purge varnish cache. This may be overkill in some cases. Hence it has been disabled unless you need it.
+		// add_action('transition_post_status', [static::class, 'onPostStatusTransition'], 10, 3);
+	}
+
+	public static function onPostSaveAndUpdate($postId)
+	{
+		do_action('pixelkey_purge_varnish');
+	}
+
+	public static function onPostStatusTransition($newStatus, $oldStatus, $post)
+	{
+		if ($newStatus == 'publish' || $newStatus == $oldStatus) return;
+		do_action('pixelkey_purge_varnish');
+	}
+}
+
+
+PostHooks::init();
+
+
+add_action('pixelkey_purge_varnish', function () {
+
+	// Set url to your Varnish server
+	$url = 'localhost';
+	$ch = curl_init($url);
+
+	// set curl to BAN
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'BAN');
+	
+	// // set the curl to Purge the cache
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Purge-Method: varnish'));
+	// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PURGE');
+	// curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+	
+	// Execute the request. IF DEBUGGING, comment out line below and uncomment "$exec_result = curl_exec($ch);"
+	curl_exec($ch);
+
+
+	// // START OF DEBUGGING SECTION
+	// // Get result of curl request. ONLY FOR DEBUGGING PURPOSES
+	// $exec_result = curl_exec($ch);
+
+	// // Check if the curl request was successful
+	// if (curl_errno($ch)) {
+	// 	// print to error file if curl request was not successful
+	// 	curl_status_log('Error: ' . curl_error($ch));
+	// } else {
+	// 	// print to error file if curl request was successful
+	// 	curl_status_log('Success: ' . $exec_result);
+	// }
+
+	// curl_status_log($exec_result);
+
+	// // END OF DEBUGGING SECTION
+
+
+	curl_close($ch);
+});
+
+
+// // START OF DEBUGGING SECTION
+
+// // Print to curl_status_log file in current plugins folder - mu-plugins folder.
+// function curl_status_log($message)
+// {
+// 	// Delete old log file
+// 	$file_name = 'curl_status_log.txt';
+// 	$file_path = plugin_dir_path(__FILE__) . $file_name;
+// 	if (file_exists($file_path)) {
+// 		unlink($file_path);
+// 	}
+// 	$log = fopen($file_path, 'a');
+// 	fwrite($log, $message . PHP_EOL);
+// 	fclose($log);
+// }
+
+// // END OF DEBUGGING SECTION
